a7306dc9e83c440c8652c488bd00c22869b42be3
simplicio10 <claypas@gmail.com>
2024-06-03 05:26:59 -0500

    Add draft results

--- df_manipulation/add_lower_names_and_positions.py
+++ df_manipulation/add_lower_names_and_positions.py
@@ -1,21 +1,34 @@
 import polars as pl
-
-df = pl.read_csv('../files/fas_13_15.csv')
-
+from datetime import datetime
+#Read in CSV with scraped player data
+df = pl.read_csv('../files/free_agents/fas_13_15.csv')
+#Split off player names into first and last, the lowercase is necessary for pybaseball player id lookup
 names = list(df['player_name'])
 split_names = [name.lower().split(' ', maxsplit=1) for name in names]
-
+#Create list of player first and last names
 first_names = [name[0] for name in split_names]
 last_names = [name[1] for name in split_names]
-
+#Add columns
 df = df.with_columns(
+    pl.col('fa_class').cast(pl.String), # Cast free agent year to string value
+    pl.col('player_name').rank(method='ordinal').alias('rank'), #Creates a unique ID value for each player
     pl.Series(first_names).alias('first_name_lower'),
     pl.Series(last_names).alias('last_name_lower'),
-    pl.when((pl.col('position') == 'RHP') | (pl.col('position') == 'LHP'))
+    pl.when((pl.col('position') == 'RHP') | (pl.col('position') == 'LHP')) #Creates general position category for batters and pitchers
     .then(pl.lit('pitcher'))
     .otherwise(pl.lit('batter'))
     .alias('normed_position')
 )
+#Convert free agent class to date format
+df = df.with_columns(
+    pl.col('fa_class').str.strptime(
+        pl.Date,
+        format='%Y', 
+        strict=False
+    ).dt.year()
+)
 
-df.write_csv('../files/fas_13_15_positions.csv')
+print(df.head(10))
+
+#df.write_csv('../files/free_agents/fas_13_15_positions.csv')
     
--- stats_lookup/get_pa_bf.py
+++ stats_lookup/get_pa_bf.py
@@ -1,8 +1,10 @@
-from pybaseball import playerid_lookup, batting_stats, pitching_stats
+from pybaseball import playerid_lookup
 import polars as pl
 
-ben_2015 = [('axford', 'john'), ('savery', 'joe'), ('romero', 'deibinson'), ('herrera', 'jonathan'), ('richard', 'clayton'),
-            ('phelps', 'cord'), ('sizemore', 'scott'), ('baxter', 'mike'), ('britton', 'buck')]
+ben_2015 = [('001', 'axford', 'john'), ('002', 'savery', 'joe'), ('003', 'romero', 'deibinson'), ('004', 'herrera', 'jonathan'), ('005', 'richard', 'clayton'),
+            ('006', 'phelps', 'cord'), ('007', 'sizemore', 'scott'), ('008', 'baxter', 'mike'), ('009', 'britton', 'buck')]
+
+players_to_id
 
 def get_id(players_list):
 
@@ -11,8 +13,8 @@ def get_id(players_list):
     for player in players_list:
         #Default blank entry if player was not in MLB
         no_mlb_entry = {
-        'name_last': player[1],
-        'name_first': player[0],
+        'name_last': player[2],
+        'name_first': player[1],
         'key_mlbam': None,
         'key_retro': None,
         'key_bbref': None,
@@ -20,21 +22,39 @@ def get_id(players_list):
         'mlb_played_first': None,
         'mlb_played_last': None
         }
-
-        id = playerid_lookup(player[0], player[1])
-
+        #Look up player id in pybaseball
+        id = playerid_lookup(player[1], player[2])
+        #Pybaseball returns a dataframe for the individual player. If the length is greater than 0 (i.e., the player has MLB experience), 
+        #convert the dataframe into Polars
         if len(id) > 0:
             valid_id = pl.from_dataframe(id)
+            valid_id = valid_id.with_columns(
+                pl.Series(player[0]).alias('native_id')
+            )
             empty_list.append(valid_id)
+        #Otherwise, create a dataframe with null values using the above dictionary
         else:
             invalid_id = pl.from_dict(no_mlb_entry)
+            invalid_id = invalid_id.with_columns(
+                pl.Series(player[0]).alias('native_id')
+            )
             empty_list.append(invalid_id)
+        #Concatenate the individual player dataframes into a single dataframe
         players = pl.concat(empty_list)
+        #Filter out the unnecessary columns
+        players = players.select(['native_id', 'name_last', 'name_first', 'key_fangraphs', 
+                                  'mlb_played_first', 'mlb_played_last'])
+        players = players.with_columns([
+            pl.col('mlb_played_first').cast(pl.String),
+            pl.col('mlb_played_last').cast(pl.String)
+        ])
 
     return players
 
 #df = get_id(ben_2015)
 #print(df)
+
+
 
 
 
