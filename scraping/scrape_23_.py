6783a0b33fe61d9fedcfce4043d52f62912828e4
simplicio10 <claypas@gmail.com>
2024-06-13 06:04:16 -0500

    Scrape 2023 BA webpage

--- scraping/scrape_23_.py
+++ scraping/scrape_23_.py
@@ -18,7 +18,7 @@ import polars as pl
 find_teams = r'\w+?\.?\s\w+\s?(Royals|Dodgers|Angel|Angels|Mets|Yankees|Padres|Giants|Cardinals|Cadinals|Jays|Sox|Rays)?\s?'
 find_players_by_position = r'([A-Z0-9]{1,3}) ([A-Za-z\. ]+?)(?=([A-Z0-9]{1,3}) |$)'
 
-webpage = 'https://www.baseballamerica.com/stories/2021-22-minor-league-free-agents-for-all-30-mlb-teams/'
+webpage = 'https://www.baseballamerica.com/stories/2022-23-minor-league-free-agents-for-all-30-mlb-teams/'
 
 #Create empty list
 all_players = []
@@ -42,18 +42,23 @@ for i in article_body_clean:
 list_of_comb_teams = []
 
 for team_idx in list_of_idxs:
-    full_string = ' '.join(article_body_clean[team_idx:team_idx + 2])
-    list_of_comb_teams.append(full_string)
+    team_idx_num = list_of_idxs.index(team_idx)
+    if team_idx_num < (len(list_of_idxs) - 1):
+        next_team = list_of_idxs[team_idx_num + 1]
+        full_string = ' '.join(article_body_clean[team_idx:next_team])
+        list_of_comb_teams.append(full_string)
+    else:
+        full_string = ' '.join(article_body_clean[team_idx:])
+        list_of_comb_teams.append(full_string)
 
 for team in list_of_comb_teams:
     player_matches = regex.findall(find_players_by_position, team)
     team_name = regex.search(find_teams, team)
     players = [{'player_name':player_name, 'position':position} for position, player_name, _ in player_matches]
     for player in players:
-        all_players.append({'player_name': player['player_name'].strip(), 'team': team_name.group(0).strip(), 'fa_class': 2022, 
+        all_players.append({'player_name': player['player_name'].strip(), 'team': team_name.group(0).strip(), 'fa_class': 2023, 
                             'position': player['position']})
 
 df = pl.DataFrame(data=all_players)
-print(article_body_clean)
 
-#df.write_csv('../files/free_agents/fas_22.csv')
+df.write_csv('../files/free_agents/fas_23.csv')
