212b279893d9467e45024a72bf1d89ff0c443547
simplicio10 <claypas@gmail.com>
2024-06-19 05:32:54 -0500

    Fix bug in regex for '24 scraping code

--- scraping/scrape_24_.py
+++ scraping/scrape_24_.py
@@ -16,7 +16,7 @@ import polars as pl
 
 # Define the regex patterns 
 find_teams = r'\w+?\.?\s[\w-’]+\s?(Royals|Dodgers|Angels|Mets|Yankees|Padres|Giants|Cardinals|Cadinals|Jays|Sox|Rays)?\s?'
-find_players_by_position = r'(?<!\w+?\.?\s[\w-’]+\s?(Royals|Dodgers|Angels|Mets|Yankees|Padres|Giants|Cardinals|Cadinals|Jays|Sox|Rays)?\s?)([A-Za-z\. ]+?), (1B|2B|3B|SS|IF|C|LF|CF|RF|OF|UTL|RHP|LHP)'
+find_players_by_position = r'(?<!\w+?\.?\s[\w-’]+\s?(Royals|Dodgers|Angels|Mets|Yankees|Padres|Giants|Cardinals|Cadinals|Jays|Sox|Rays)?\s?)([A-Za-z\. ]+?), (1B|2B|3B|SS|IF|C(F)?|LF|RF|OF|UTL|RHP|LHP)'
 
 webpage = 'https://www.baseballamerica.com/stories/2023-milb-free-agents/'
 
@@ -48,11 +48,11 @@ for team_idx in list_of_idxs:
 for team in list_of_comb_teams:
     player_matches = regex.findall(find_players_by_position, team)
     team_name = regex.search(find_teams, team)
-    players = [{'player_name':player_name, 'position':position} for _, player_name, position in player_matches]
+    players = [{'player_name':player_name, 'position':position} for _, player_name, position, _ in player_matches]
     for player in players:
         all_players.append({'player_name': player['player_name'].strip(), 'team': team_name.group(0).strip(), 'fa_class': 2024, 
-                            'position': player['position']})
-
+                            'position': player['position']}) 
+                            
 df = pl.DataFrame(data=all_players)    
 
-df.write_csv('../files/free_agents/fas_24.csv')
+df.write_csv('../files/scraped_free_agents/fas_24.csv')
