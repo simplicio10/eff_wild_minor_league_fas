9caf30382e5d7254d0a554aa23d7bff6eda3f0ff
simplicio10 <claypas@gmail.com>
2024-06-30 06:12:16 -0500

    Change columns in stats files

--- files/stats/get_stats.py
+++ files/stats/get_stats.py
@@ -6,18 +6,18 @@ from pybaseball import batting_stats, pitching_stats
 import polars as pl
 
 batting_14_18 = pl.DataFrame(batting_stats(2014, 2018, qual=0, ind=1))
-batting_14_18 = batting_14_18.select(['IDfg', 'Season', 'Name', 'Team', 'Age', 'G', 'PA'])
+batting_14_18 = batting_14_18.select(['IDfg', 'Season', 'Age', 'G', 'PA'])
 
 batting_19_23 = pl.DataFrame(batting_stats(2019, 2023, qual=0, ind=1))
-batting_19_23 = batting_19_23.select(['IDfg', 'Season', 'Name', 'Team', 'Age', 'G', 'PA'])
+batting_19_23 = batting_19_23.select(['IDfg', 'Season', 'Age', 'G', 'PA'])
 
 pitching_14_18 = pl.DataFrame(pitching_stats(2014, 2018, qual=0, ind=1))
-pitching_14_18 = pitching_14_18.select(['IDfg', 'Season', 'Name', 'Team', 'Age', 'G', 'TBF'])
+pitching_14_18 = pitching_14_18.select(['IDfg', 'Season', 'Age', 'G', 'TBF'])
 
 pitching_19_23 = pl.DataFrame(pitching_stats(2019, 2023, qual=0, ind=1))
-pitching_19_23 = pitching_19_23.select(['IDfg', 'Season', 'Name', 'Team', 'Age', 'G', 'TBF'])
+pitching_19_23 = pitching_19_23.select(['IDfg', 'Season', 'Age', 'G', 'TBF'])
 
-batting_14_18.write_csv('../files/stats/batting_14_18.csv')
-batting_19_23.write_csv('../files/stats/batting_19_23.csv')
-pitching_14_18.write_csv('../files/stats/pitching_14_18.csv')
-pitching_19_23.write_csv('../files/stats/pitching_19_23.csv')
+batting_14_18.write_csv('batting_14_18.csv')
+batting_19_23.write_csv('batting_19_23.csv')
+pitching_14_18.write_csv('pitching_14_18.csv')
+pitching_19_23.write_csv('pitching_19_23.csv')
