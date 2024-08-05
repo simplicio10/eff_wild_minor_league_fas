6f8a91eb61ea3ff77a0b41918e14ca7fb7284760
simplicio10 <claypas@gmail.com>
2024-08-05 06:13:38 -0500

    Modify dataframe manipulation file

--- df_manipulation/add_lower_names_and_positions.py
+++ df_manipulation/add_lower_names_and_positions.py
@@ -13,7 +13,7 @@ df = pl.concat(dfs, how="diagonal").select(columns).collect()
 
 #Split off player names into first and last, the lowercase is necessary for pybaseball player id lookup
 names = list(df['player_name'])
-split_names = [name.lower().split(' ', maxsplit=1) for name in names]
+split_names = [name.split(' ', maxsplit=1) for name in names]
 
 #Create list of player first and last names
 first_names = [name[0] for name in split_names]
@@ -27,8 +27,8 @@ df = df.join(team_names, on='team')
 df = df.with_columns(
     pl.col('fa_class').cast(pl.String), # Cast free agent year to string value
     pl.col('player_name').rank(method='ordinal').cast(pl.String).alias('internal_id'), #Creates a unique ID value for each player
-    pl.Series(first_names).alias('first_name_lower'),
-    pl.Series(last_names).alias('last_name_lower'),
+    pl.Series(first_names).alias('first_name'),
+    pl.Series(last_names).alias('last_name'),
     pl.when((pl.col('position') == 'RHP') | (pl.col('position') == 'LHP')) #Creates general position category for batters and pitchers
     .then(pl.lit('pitcher'))
     .when((pl.col('position').is_null()))
@@ -36,21 +36,13 @@ df = df.with_columns(
     .otherwise(pl.lit('batter'))
     .alias('normed_position')
 )
-#Convert free agent class to date format
-df = df.with_columns(
-    pl.col('fa_class').str.strptime(
-        pl.Date,
-        format='%Y', 
-        strict=False
-    )
-)
 
 #Reorder columns
 df = df.select(
     pl.col('internal_id'),
     pl.col('player_name'),
-    pl.col('first_name_lower'),
-    pl.col('last_name_lower'),
+    pl.col('first_name'),
+    pl.col('last_name'),
     pl.col('team_abbreviation').alias('team'),
     pl.col('fa_class'),
     pl.col('position'),
