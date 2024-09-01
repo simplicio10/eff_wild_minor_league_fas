2669076f0b1b3c30fa449dde79f3992466ef8ba2
simplicio10 <claypas@gmail.com>
2024-09-01 09:13:54 -0500

    Refactor code for lazyFrame functionality

--- player_id_lookup/get_ids.py
+++ player_id_lookup/get_ids.py
@@ -1,17 +1,27 @@
 import polars as pl
 import sys
+import random
 sys.path.insert(0, '/home/salviati/projects/baseball/custom_player_lookup/script')
 import id_lookup_tool as player_id_lookup
 
-fas_list = pl.read_csv('../files/free_agents/final/fas_to_check_cleaned.csv', schema_overrides={'internal_id': pl.String})
-stats = pl.read_csv('../files/stats/player_stats.csv')
+fas_list = pl.scan_csv('../files/free_agents/final/fas_to_check_cleaned.csv', schema_overrides={'internal_id': pl.String})
+stats = pl.scan_csv('../files/stats/player_stats.csv')
 stats = stats.rename({'IDfg': 'key_fangraphs'})
 
 #Testing
-partial = fas_list.sample(n=25)
+# Use a random filter to create a sample LazyFrame
+total_rows = fas_list.collect().shape[0]
+sample_size = 25
+random.seed(42)  # for reproducibility
+random_indices = set(random.sample(range(total_rows), sample_size))
 
-def process_valid_id(valid_id, player) -> pl.DataFrame:
-    if valid_id.height > 1:
+partial = fas_list.with_row_index().filter(
+    pl.col("index")
+    .is_in(random_indices)
+    ).drop("index")
+
+def process_valid_id(valid_id, player) -> pl.LazyFrame:
+    if valid_id.collect().height > 1:
         valid_id = valid_id.with_columns(
             full_name=pl.lit(player['player_name']),
             team=pl.lit(player['team']),
@@ -20,7 +30,7 @@ def process_valid_id(valid_id, player) -> pl.DataFrame:
             position_cat=pl.lit(player['normed_position']),
             chadwick_returned=pl.lit('multiple_passed_bref_check')
         )
-    elif valid_id.height == 1:
+    elif valid_id.collect().height == 1:
         valid_id = valid_id.with_columns(
             full_name=pl.lit(player['player_name']),
             team=pl.lit(player['team']),
@@ -40,8 +50,7 @@ def process_valid_id(valid_id, player) -> pl.DataFrame:
         )
     return valid_id
 
-def get_id(df) -> pl.DataFrame:
-
+def get_id(df) -> pl.LazyFrame:
     final_players_list = []
 
     players = df.select('internal_id', 
@@ -49,18 +58,17 @@ def get_id(df) -> pl.DataFrame:
                         'player_name', 
                         'last_name', 
                         'first_name', 
-                        'fa_class', 
-                        'normed_position').to_dicts()
+                        'fa_class',
+                        'position', 
+                        'normed_position').collect().to_dicts()
 
     for player in players:
-
         if 'Jr' in player['last_name']:
             player['last_name'] = player['last_name'].removesuffix(' Jr')
         
         if 'Jr.' in player['last_name']:
             player['last_name'] = player['last_name'].removesuffix(' Jr.')
 
-        #Default blank entry if player not found
         no_entry = {
             'full_name': player['player_name'],
             'team': player['team'],
@@ -76,59 +84,57 @@ def get_id(df) -> pl.DataFrame:
             'pro_played_last': None,
             'internal_id': player['internal_id'],
             'fa_class': player['fa_class'],
+            'position': player['position'],
             'position_cat': player['normed_position'],
             'chadwick_returned': 'zero'
-            }       
-        #Look up player id in custom script
-        if player['fa_class'] == 2021: 
-            active_year = 2019 #Correction because no minor league season in 2020
-        else:
-            active_year = player['fa_class'] - 1
-        id = pl.DataFrame(player_id_lookup.player_info_lookup(
+        }       
+
+        active_year = 2019 if player['fa_class'] == 2021 else player['fa_class'] - 1
+        
+        id = pl.LazyFrame(player_id_lookup.player_info_lookup(
             player['last_name'], 
             player['first_name'], 
             year_active=active_year, 
             check_accents=True,
             phonetic_match=True))    
-        #Function returns a dataframe for the individual player. If the player has MLB experience (len(df) > 0), convert the dataframe into Polars.
-        if id.height == 1:
+
+        if id.collect().height == 1:
             valid_id = process_valid_id(id, player)
             final_players_list.append(valid_id)
-        elif id.height > 1:
+        elif id.collect().height > 1:
             try:
-                valid_id = player_id_lookup.ml_affiliate_filter(
-                    id,
+                valid_id = pl.LazyFrame(player_id_lookup.ml_affiliate_filter(
+                    id.collect(),
                     active_year,
                     player['team']
-                )
+                ))
             except:
                 valid_id = id            
             valid_id = process_valid_id(valid_id, player)
             final_players_list.append(valid_id)
-        #Otherwise, create a dataframe with null values using the no_entry variable
         else:
             years_back = 1
             while years_back < 9:
                 active_year = active_year - years_back
 
-                id = pl.DataFrame(player_id_lookup.player_info_lookup(
+                id = pl.LazyFrame(player_id_lookup.player_info_lookup(
                     player['last_name'], 
                     player['first_name'], 
                     year_active=active_year, 
                     check_accents=True,
                     phonetic_match=True))      
                          
-                if id.height == 1:
+                if id.collect().height == 1:
                     valid_id = process_valid_id(id, player)
                     final_players_list.append(valid_id)
                     break
-                elif id.height > 1:
+                elif id.collect().height > 1:
                     try:
-                        valid_id = player_id_lookup.ml_affiliate_filter(
-                            id,
+                        valid_id = pl.LazyFrame(player_id_lookup.ml_affiliate_filter(
+                            id.collect(),
                             active_year,
                             player['team']
-                        )
+                        ))
                     except:
                         valid_id = id                 
                     valid_id = process_valid_id(valid_id, player)
@@ -138,21 +144,18 @@ def get_id(df) -> pl.DataFrame:
                     years_back += 1
 
             if years_back == 9:
-                invalid_id = pl.from_dict(no_entry)
+                invalid_id = pl.LazyFrame(no_entry)
                 final_players_list.append(invalid_id)
 
-    #Concatenate the individual player dataframes into a single dataframe
     players = pl.concat(final_players_list, how='diagonal_relaxed')
-    #Filter out unnecessary columns
     players = players.select(['internal_id', 'full_name', 'team', 'name_last', 'name_first', 'fa_class', 'key_fangraphs', 
                             'key_bbref_minors','mlb_played_first', 'mlb_played_last', 'pro_played_first', 'pro_played_last',
                             'position', 'position_cat', 'chadwick_returned'])
 
     return players
 
-id = get_id(fas_list)
-#id = get_id(partial)
-id.write_csv('../files/free_agents/final/fa_id_matched.csv')
+id = get_id(partial)
+id.collect().write_csv('../files/free_agents/final/lazy_test.csv')
 
 
 '''players_and_stats = id.lazy().collect().join(stats.lazy().collect(), on='key_fangraphs', how='left')
