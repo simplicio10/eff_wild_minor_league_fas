de5e62853c2bc01ad0a43b555e760a6e70c74beb
simplicio10 <claypas@gmail.com>
2024-11-25 04:50:09 -0600

    Update draft results for 2024

--- player_id_lookup/get_draft_ids.py
+++ player_id_lookup/get_draft_ids.py
@@ -3,9 +3,8 @@ import sys
 sys.path.insert(0, '/home/salviati/projects/baseball/custom_player_lookup/script')
 import id_lookup_tool as player_id_lookup
 
-players = pl.scan_csv('../files/free_agents/final/fas_final.csv')
-players = players.rename({'fa_class': 'draft_year'})
-draft_results = pl.scan_csv('../files/draft_results/draft_results_w_ids.csv')
+players = pl.scan_csv('../files/free_agents/final/fas_final.csv').rename({'fa_class': 'draft_year'})
+draft_results = pl.scan_csv('../files/draft_results/draft_results.csv').rename({'team': 'contestant', 'PA_TBF': 'pa_tbf'})
 draft_results = draft_results.with_columns([
     pl.col('player').str.splitn(by=' ', n=2)
     .alias('name_struct')
@@ -15,6 +14,6 @@ draft_results = draft_results.with_columns([
 ]).drop('name_struct')
 
 draft_combined = draft_results.join(players, on=['name_first', 'name_last', 'draft_year'], how='left')
-draft_combined = draft_combined.select('player', 'pa_tbf', 'contestant', 'draft_year', 'key_bbref_minors')
+draft_combined = draft_combined.select('player', 'pa_tbf', 'contestant', 'draft_year', 'key_bbref_minors', 'key_fangraphs')
 
-draft_combined.collect().write_csv('../files/draft_results/test_join.csv')
+draft_combined.collect().write_csv('../files/draft_results/draft_results_w_both_ids.csv')
